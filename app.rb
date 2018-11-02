7ed86a296cfccdaa5328040886aa64bd75d2c3ed
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-02 13:49:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
     multiply = params[:number1].to_i * params[:number2].to_i
     divide   = params[:number1].to_i / params[:number2].to_i
     if params[:operation] == "add" 
-        #{params[:number1]}.to_i + #{params[:number2]}.to_i
+        "#{num1} + #{num2}
     elsif params[:operation] == "subtract"
         #{subtract}
     elsif params[:operation] == "multiply"
