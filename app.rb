f76e88d076892b4864b8dd0f290a407e83e70629
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-02 13:40:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,8 +29,8 @@ class App < Sinatra::Base
     # multiplies two numbers together
     # divides the first number by the second number
     
-    # num1 = params[:number1].to_i
-    # num2 = params[:number2].to_i
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
     
     case params[:operation] 
     when "add" 
